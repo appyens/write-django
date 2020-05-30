a29532349a358bce6d31caf68ccbd5d7f46c7903
appyens <an.anurag@msn.com>
2020-05-31 03:39:58 +0530

    share email added

--- post/forms.py
+++ post/forms.py
@@ -7,14 +7,25 @@ class AddCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control col-4'}),
-            'email': forms.TextInput(attrs={'class': 'form-control col-4'}),
-            'body': forms.TextInput(attrs={'class': 'form-control col-4'})
-        }
+
+    def __init__(self, *args, **kwargs):
+        super(AddCommentForm, self).__init__(*args, **kwargs)
+
+        for name in self.fields.keys():
+            self.fields[name].widget.attrs.update({
+                'class': 'form-control col-4',
+            })
 
 
 class EmailForm(forms.Form):
 
-    to_id = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    to_name = forms.CharField(widget=forms.TimeInput(attrs={'class': 'form-control'}))
+    to_id = forms.EmailField(widget=forms.EmailInput())
+    to_name = forms.CharField(widget=forms.TimeInput())
+
+    def __init__(self, *args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+
+        for name in self.fields.keys():
+            self.fields[name].widget.attrs.update({
+                'class': 'form-control col-4',
+            })
